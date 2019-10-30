9f1f1fddfdb7f2289591d1b08ceca916ac7776b9
Jel Vankan <jelvankan@hotmail.nl>
2019-10-30 17:07:05 +0100

    Latest version of Chinese Checkers with updated GUI

---
+++ GraphOscar.java
@@ -0,0 +1,490 @@
+import org.w3c.dom.CDATASection;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class GraphOscar {
+
+    /*
+     *   Creates a chinese checkers board as the form of a graph containing
+     *
+     */
+    private static Node[] nodeList;
+    public Color lightBlue = new Color(0,0,182,155);
+    public int numPly; // Number of players
+
+    private static Node
+            AA,
+            BA, BB,
+            CA, CB, CC,
+            DA, DB, DC, DD,
+            E4, E3, E2, E1, EA, EB, EC, ED, EE, EF, EG, EH, EI,
+            F3, F2, F1, FA, FB, FC, FD, FE, FF, FG, FH, FI,
+            G2, G1, GA, GB, GC, GD, GE, GF, GG, GH, GI,
+            H1, HA, HB, HC, HD, HE, HF, HG, HH, HI,
+            IA, IB, IC, ID, IE, IF, IG, IH, II,
+            J1, JA, JB, JC, JD, JE, JF, JG, JH, JI,
+            K2, K1, KA, KB, KC, KD, KE, KF, KG, KH, KI,
+            L3, L2, L1, LA, LB, LC, LD, LE, LF, LG, LH, LI,
+            M4, M3, M2, M1, MA, MB, MC, MD, ME, MF, MG, MH, MI,
+            NA, NB, NC, ND,
+            OA, OB, OC,
+            PA, PB,
+            QA;
+
+    public static void main(String args[]){
+
+        makeNodes();
+        makeNodeList();
+        connectEdges();
+        testNode(nodeList[50]); //test adjacent nodes
+        testNodeColor(EG);      //test the color of a node by printing it
+        testNode(GE);
+    }
+
+    public GraphOscar(int n){
+        this.numPly = n;
+        makeNodes();
+        makeNodeList();
+        connectEdges();
+        initializeStartingPositions(n);
+    }
+
+    public static void testNode(Node node){
+        /*
+         *   Provides the adjacent nodes
+         *   Useful to check connections
+         */
+        if(node.getLabel() == null){System.out.println("This is not a valid node.");}
+        else{
+
+            if(!node.getUpRight().getLabel().equals("null")){
+                System.out.println("The upRight node of " + node.getLabel() + " is " + node.getUpRight().getLabel());
+            }
+            else{System.out.println("There is no upRight node for " + node.getLabel());}
+
+            if(!node.getRight().getLabel().equals("null")){
+                System.out.println("The Right node of " + node.getLabel() + " is " + node.getRight().getLabel());
+            }
+            else{System.out.println("There is no Right node for " + node.getLabel());}
+
+            if(!node.getdRight().getLabel().equals("null")){
+                System.out.println("The dRight node of " + node.getLabel() + " is " + node.getdRight().getLabel());
+            }
+            else{System.out.println("There is no dRight node for " + node.getLabel());}
+
+            if(!node.getdLeft().getLabel().equals("null")){
+                System.out.println("The dLeft node of " + node.getLabel() + " is " + node.getdLeft().getLabel());
+            }
+            else{System.out.println("There is no dLeft node for " + node.getLabel());}
+
+            if(!node.getLeft().getLabel().equals("null")){
+                System.out.println("The Left node of " + node.getLabel() + " is " + node.getLeft().getLabel());
+            }
+            else{System.out.println("There is no Left node for " + node.getLabel());}
+
+            if(!node.getUpLeft().getLabel().equals("null")){
+                System.out.println("The UpLeft node of " + node.getLabel() + " is " + node.getUpLeft().getLabel());
+            }
+            else{System.out.println("There is no UpLeft node for " + node.getLabel());}
+
+        }
+
+    }
+
+    public static void testNodeColor(Node node){
+
+        System.out.println("-------------------------");
+        System.out.println(node.getColor());
+        System.out.println("-------------------------");
+
+
+    }
+
+    private static void makeNodes(){
+
+        AA = new Node("AA",0,0);
+
+        BA = new Node("BA",1,0);
+        BB = new Node("BB",1,1);
+
+        CA = new Node("CA",2,0);
+        CB = new Node("CB",2,1);
+        CC = new Node("CC",2,2);
+
+        DA = new Node("DA",3,0);
+        DB = new Node("DB",3,1);
+        DC = new Node("DC",3,2);
+        DD = new Node("DD",3,3);
+
+        EA = new Node("EA",4,0);
+        EB = new Node("EB",4,1);
+        EC = new Node("EC",4,2);
+        ED = new Node("ED",4,3);
+        EE = new Node("EE",4,4);
+
+        EF = new Node("EF",4, 5);
+        EG = new Node("EG",4, 6);
+        EH = new Node("EH",4, 7);
+        EI = new Node("EI",4, 8);
+
+        E1 = new Node("E1",4, -1);
+        E2 = new Node("E2",4, -2);
+        E3 = new Node("E3",4, -3);
+        E4 = new Node("E4",4, -4);
+
+        FA = new Node("FA",5,0);
+        FB = new Node("FB",5,1);
+        FC = new Node("FC",5,2);
+        FD = new Node("FD",5,3);
+        FE = new Node("FE",5,4);
+        FF = new Node("FF",5,5);
+
+        FG = new Node("FG",5, 6);
+        FH = new Node("FH",5, 7);
+        FI = new Node("FI",5, 8);
+
+        F1 = new Node("F1",5, -1);
+        F2 = new Node("F2",5, -2);
+        F3 = new Node("F3",5, -3);
+
+        GA = new Node("GA",6,0);
+        GB = new Node("GB",6,1);
+        GC = new Node("GC",6,2);
+        GD = new Node("GD",6,3);
+        GE = new Node("GE",6,4);
+        GF = new Node("GF",6,5);
+        GG = new Node("GG",6,6);
+
+        GH = new Node("GH",6,7);
+        GI = new Node("GI",6,8);
+
+        G1 = new Node("G1",6,-1);
+        G2 = new Node("G2",6,-2);
+
+        HA = new Node("HA",7,0);
+        HB = new Node("HB",7,1);
+        HC = new Node("HC",7,2);
+        HD = new Node("HD",7,3);
+        HE = new Node("HE",7,4);
+        HF = new Node("HF",7,5);
+        HG = new Node("HG",7,6);
+        HH = new Node("HH",7,7);
+
+        HI = new Node("HH",7,8);
+
+        H1 = new Node("H1",7, -1);
+
+        IA = new Node("IA",8,0);
+        IB = new Node("IB",8,1);
+        IC = new Node("IC",8,2);
+        ID = new Node("ID",8,3);
+        IE = new Node("IE",8,4);
+        IF = new Node("IF",8,5);
+        IG = new Node("IG",8,6);
+        IH = new Node("IH",8,7);
+        II = new Node("II",8,8);
+
+        JA = new Node("JA",9,0);
+        JB = new Node("JB",9,1);
+        JC = new Node("JC",9,2);
+        JD = new Node("JD",9,3);
+        JE = new Node("JE",9,4);
+        JF = new Node("JF",9,5);
+        JG = new Node("JG",9,6);
+        JH = new Node("JH",9,7);
+
+        JI = new Node("JI",9, 8);
+
+        J1 = new Node("J1",9, -1);
+
+        KA = new Node("KA",10,0);
+        KB = new Node("KB",10,1);
+        KC = new Node("KC",10,2);
+        KD = new Node("KD",10,3);
+        KE = new Node("KE",10,4);
+        KF = new Node("KF",10,5);
+        KG = new Node("KG",10,6);
+
+        KH = new Node("KH",10,7);
+        KI = new Node("KI",10,8);
+
+        K1 = new Node("K1",10,-1);
+        K2 = new Node("K2",10,-2);
+
+        LA = new Node("LA",11,0);
+        LB = new Node("LB",11,1);
+        LC = new Node("LC",11,2);
+        LD = new Node("LD",11,3);
+        LE = new Node("LE",11,4);
+        LF = new Node("LF",11,5);
+
+        LG = new Node("LG",11,6);
+        LH = new Node("LH",11,7);
+        LI = new Node("LI",11,8);
+
+        L1 = new Node("L1",11,-1);
+        L2 = new Node("L2",11,-2);
+        L3 = new Node("L3",11,-3);
+
+        MA = new Node("MA",12,0);
+        MB = new Node("MB",12,1);
+        MC = new Node("MC",12,2);
+        MD = new Node("MD",12,3);
+        ME = new Node("ME",12,4);
+
+        MF = new Node("MF",12,5);
+        MG = new Node("MG",12,6);
+        MH = new Node("MH",12,7);
+        MI = new Node("MI",12,8);
+
+        M1 = new Node("M1",12,-1);
+        M2 = new Node("M2",12,-2);
+        M3 = new Node("M3",12,-3);
+        M4 = new Node("M4",12,-4);
+
+        NA = new Node("NA",13,0);
+        NB = new Node("NB",13,1);
+        NC = new Node("NB",13,2);
+        ND = new Node("NB",13,3);
+
+        OA = new Node("OA",14,0);
+        OB = new Node("OB",14,1);
+        OC = new Node("OC",14,2);
+
+        PA = new Node("PA",15,0);
+        PB = new Node("PB",15,1);
+
+        QA = new Node("QA",16,0);
+    }
+
+    private static void makeNodeList(){
+
+        nodeList = new Node[]{
+                AA,
+                BA, BB,
+                CA, CB, CC,
+                DA, DB, DC, DD,
+                EA, EB, EC, ED, EE, EF, EG, EH, EI, E1, E2, E3, E4,
+                FA, FB, FC, FD, FE, FF, FG, FH, FI, F1, F2, F3,
+                GA, GB, GC, GD, GE, GF, GG, GH, GI, G1, G2,
+                HA, HB, HC, HD, HE, HF, HG, HH, HI, H1,
+                IA, IB, IC, ID, IE, IF, IG, IH, II,
+                JA, JB, JC, JD, JE, JF, JG, JH, JI, J1,
+                KA, KB, KC, KD, KE, KF, KG, KH, KI, K1, K2,
+                LA, LB, LC, LD, LE, LF, LG, LH, LI, L1, L2, L3,
+                MA, MB, MC, MD, ME, MF, MG, MH, MI, M1, M2, M3, M4,
+                NA, NB, NC, ND,
+                OA, OB, OC,
+                PA, PB,
+                QA};
+
+
+    }
+
+    private static void connectEdges(){
+        /*
+         *   Method to connect the graph
+         */
+        for(int i=0; i<nodeList.length; i++){
+            for(int j=0; j<nodeList.length; j++){
+                nodeList[i].addEdge(nodeList[j]);
+            }
+        }
+    }
+
+    private static void initializeStartingPositions(int n){
+        if(n==2){
+            for(int i = 0; i < 10; i++){
+                nodeList[i].setColor(Color.BLUE);
+                nodeList[120-i].setColor(Color.RED);
+            }
+        }
+        else if(n==6){
+            //PLAYER 1 AND 4
+            for(int i = 0; i < 10; i++){
+                nodeList[i].setColor(Color.BLUE);
+                nodeList[120-i].setColor(Color.RED);
+            }
+
+            //START PLAYER 2 and 6///
+            for(int j=0;j<4;j++) {
+                nodeList[15 + j].setColor(Color.GRAY);
+                nodeList[22 - j].setColor(Color.GREEN);
+            }
+
+            for(int j=0;j<3;j++) {
+                nodeList[29 + j].setColor(Color.GRAY);
+                nodeList[34 - j].setColor(Color.GREEN);
+            }
+
+            for(int j=0;j<2;j++) {
+                nodeList[42 + j].setColor(Color.GRAY);
+                nodeList[45 - j].setColor(Color.GREEN);
+            }
+
+            nodeList[54].setColor(Color.GRAY);
+            nodeList[55].setColor(Color.GREEN);
+            ///END PLAYER 2 and 6///////////
+
+            ///START PLAYER 3 and 5/////////
+            for(int k=0; k < 4; k++) {
+                nodeList[103 + k].setColor(Color.ORANGE);
+                nodeList[110 - k].setColor(Color.BLACK);
+            }
+
+            for(int k=0; k < 3; k++) {
+                nodeList[92 + k].setColor(Color.ORANGE);
+                nodeList[97 - k].setColor(Color.BLACK);
+            }
+
+            for(int k=0; k < 2; k++) {
+                nodeList[82 + k].setColor(Color.ORANGE);
+                nodeList[85 - k].setColor(Color.BLACK);
+            }
+               nodeList[73].setColor(Color.ORANGE);
+               nodeList[74].setColor(Color.BLACK);
+        }
+        ////END PLAYER 3 and 5///////////
+        else if(n==4){
+
+            ///START PLAYER 3 and 5/////////
+            for(int k=0; k < 4; k++) {
+                nodeList[103 + k].setColor(Color.ORANGE);
+                nodeList[110 - k].setColor(Color.BLACK);
+            }
+
+
+            for(int k=0; k < 3; k++) {
+                nodeList[92 + k].setColor(Color.ORANGE);
+                nodeList[97 - k].setColor(Color.BLACK);
+            }
+
+            for(int k=0; k < 2; k++) {
+
+                nodeList[82 + k].setColor(Color.ORANGE);
+                nodeList[85 - k].setColor(Color.BLACK);
+            }
+
+               nodeList[73].setColor(Color.ORANGE);
+               nodeList[74].setColor(Color.BLACK);
+
+        ////END PLAYER 3 and 5///////////
+
+        //START PLAYER 2 and 6///
+        for(int j=0;j<4;j++) {
+            nodeList[15 + j].setColor(Color.GRAY);
+            nodeList[22 - j].setColor(Color.GREEN);
+        }
+
+        for(int j=0;j<3;j++) {
+            nodeList[29 + j].setColor(Color.GRAY);
+            nodeList[34 - j].setColor(Color.GREEN);
+        }
+
+        for(int j=0;j<2;j++) {
+            nodeList[42 + j].setColor(Color.GRAY);
+            nodeList[45 - j].setColor(Color.GREEN);
+        }
+
+        nodeList[54].setColor(Color.GRAY);
+        nodeList[55].setColor(Color.GREEN);
+        ///END PLAYER 2 and 6///////////
+
+    }
+        else{
+            //PLAYER 1
+            for(int i = 0; i < 10; i++){
+                nodeList[i].setColor(Color.BLUE);
+            }
+            for(int k=0; k < 4; k++) {
+                nodeList[103 + k].setColor(Color.ORANGE);
+                nodeList[110 - k].setColor(Color.BLACK);
+            }
+
+
+            for(int k=0; k < 3; k++) {
+                nodeList[92 + k].setColor(Color.ORANGE);
+                nodeList[97 - k].setColor(Color.BLACK);
+            }
+
+            for(int k=0; k < 2; k++) {
+                nodeList[82 + k].setColor(Color.ORANGE);
+                nodeList[85 - k].setColor(Color.BLACK);
+            }
+               nodeList[73].setColor(Color.ORANGE);
+               nodeList[74].setColor(Color.BLACK);
+        }
+    }
+
+
+    public String getNodeLabel(int node){
+        return nodeList[node].getLabel();
+    }
+    public Node getSecNode(int node){
+        return nodeList[node];
+    }
+    public Color getNodeColor(int node){
+        return nodeList[node].getColor();
+    }
+    public int getNodeXCoords(int node){
+        return nodeList[node].getX();
+    }
+    public int getNodeYCoords(int node){
+        return nodeList[node].getY();
+    }
+    public Node[] getNodes(){
+        return this.nodeList;
+    }
+
+    //Oscar
+
+    public static ArrayList<Node> oG = new ArrayList<Node>();
+
+    public static ArrayList<Node> popularChoice(Node n){
+        oG.clear();
+        Node[] availableNodes = n.adjN(); //Gets adjacent nodes from node n
+        for(int i = 0; i < availableNodes.length; i++){ // Goes through all neighbour nodes of n
+            choose(availableNodes[i], i);
+        }
+        return oG;
+    }
+
+    public static void choose(Node n, int i){
+        Node[] av = n.adjN();
+        if(!n.isOccupied()){ //if node n is not occupied, it is added to the list of available spots
+            oG.add(n);
+        }
+        else if(!av[i].isOccupied()){
+                oG.add(av[i]); //if node n is occupied, we check whether the node which lies in the i-direction is available
+                isItGucci(av[i], i);
+        }
+
+    }
+
+    public static void isItGucci(Node n, int m){
+        Node[] y = n.adjN(); //gets neighbours of node n
+        for(int i=0; i<y.length; i++){
+            if(y[i].getLabel()==null){
+                //Do nothing
+            }
+            if(y[i].isOccupied() && (i+m)!=5){ //The (i+m)!=5 statement is to make sure that the piece doesn't jump back to its original position
+                if(y[i].getAdj(i).getLabel()==null){
+                    //Do nothing
+                }
+                if(!y[i].getAdj(i).isOccupied()){ //
+                    if(!oG.contains(y[i].getAdj(i))) {
+                        oG.add(y[i].getAdj(i));
+                        isItGucci(y[i].getAdj(i), i);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void display(){
+        for (int i=0; i<oG.size();i++ ) {
+            System.out.println("possible move "+oG.get(i).getLabel());
+        }
+    }
+
+}
---
+++ introMenu.java
@@ -0,0 +1,110 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+
+class introMenu {
+
+    private static JFrame frame;
+    private static GUI6players disp;
+    private static String guide = "Chinese Checkers is a game in which players race each other to see who can fill their destination triangle with colored pegs first. \n While the game is neither Chinese nor Checkers, it's a fun tactical game invented in Germany but based on an American game called Halma.\n You can play the game with two to six players.";
+
+
+    private static void helpScreen() {
+        JDialog dialog = new JDialog(new Frame(), "Help");
+        dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JTextArea text = new JTextArea(guide);
+        text.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        text.setEditable(false);
+        dialog.setLocationRelativeTo(null);
+        dialog.add(text);
+        dialog.pack();
+        dialog.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+
+            final JFrame frame = new JFrame("CHINESE CHECKERS");
+            frame.setLocationRelativeTo(null);
+
+            final JPanel status_panel = new JPanel();
+            final JPanel gamestats_panel = new JPanel();
+
+            gamestats_panel.setLayout(new GridLayout(3,21));
+            frame.add(status_panel, BorderLayout.SOUTH);
+            frame.add(gamestats_panel, BorderLayout.EAST);
+
+            final JLabel status = new JLabel("Start Game - Player 1 begins, then in clockwise order");
+
+            Font font = new Font("Serif", Font.BOLD, 25);
+
+            status_panel.add(status);
+
+
+            // start buttons
+            final JPanel control_panel = new JPanel();
+            frame.add(control_panel, BorderLayout.NORTH);
+
+
+            final JButton start1v1 = new JButton("2 Players");
+            start1v1.addActionListener(e -> {
+                frame.setVisible(false);
+                new CCheckers(2);
+            });
+
+
+            final JButton start1v2 = new JButton("3 Players");
+            start1v2.addActionListener(e -> {
+                frame.setVisible(false);
+                new CCheckers(3);
+            });
+
+
+            final JButton start4v4 = new JButton("4 Players");
+            start4v4.addActionListener(e -> {
+                frame.setVisible(false);
+                new CCheckers(4);
+            });
+
+
+            final JButton start6players = new JButton("6 Players");
+            start6players.addActionListener(e -> {
+                frame.setVisible(false);
+                new CCheckers(6);
+            });
+
+
+        control_panel.add(start1v1);
+        control_panel.add(start1v2);
+        control_panel.add(start4v4);
+        control_panel.add(start6players);
+
+            final JButton help = new JButton("Help");
+            help.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    helpScreen();
+                }
+            });
+            control_panel.add(help);
+
+
+            frame.pack();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+
+        }
+
+
+    public static void changeScene(){
+        // disp = new DisplayEarth(true, 2000);
+        disp.setLayout(new GridLayout(0, 10));
+
+        //ads the GUI to the frame
+        frame.setContentPane(disp);
+        frame.setVisible(true);
+    }
+
+    }
