c362a4d7d46b5df62d557215a0d14e1fd71852d8
jelvankan <55206703+jelvankan@users.noreply.github.com>
2019-11-16 21:41:39 +0100

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,245 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+
+public class GUI extends JComponent {
+
+    //Jel
+    int interval = 45;
+    int diameter = 40;
+
+
+    Graph board;
+    ArrayList<Color> colorsOfPlayers = new ArrayList<Color>();
+    Node[] nodeList;
+    int selectedNode, previousSelectedNode = -1;
+    public static boolean firstMove = false;
+    static long start = System.nanoTime();
+    int currentPlayer = 1;
+    int turnsCount = 1;
+    int nrsP;
+
+    boolean player1 = true;
+
+    public GUI() {
+
+        this.board = new Graph();
+        this.nodeList = board.getNodes();
+
+        MousePressListener var1 = new MousePressListener();
+        this.addMouseListener(var1);
+
+    }
+
+
+    public void paintComponent(Graphics g) {
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        long finish = System.nanoTime();
+        long timeElapsed = finish - start;
+
+        if (timeElapsed % 1000000000 == 0)
+            repaint();
+
+        GradientPaint gp1 = new GradientPaint(50, 1, Color.blue, 20, 20, Color.lightGray, true);
+        GradientPaint gp2 = new GradientPaint(100, 100, Color.green, 5, 5, Color.lightGray, true);
+        GradientPaint gp3 = new GradientPaint(45, 45, Color.yellow, 70, 70, Color.orange, true);
+        GradientPaint gp4 = new GradientPaint(5, 5, Color.darkGray, 20, 20, Color.red, true);
+
+
+        if (player1) {
+            g2.setPaint(Color.BLUE);
+            g2.drawString("It is Player's Blue turn", 1000, 100);
+        } else {
+            g2.setPaint(Color.RED);
+            g2.drawString("It is Player's Red turn", 1000, 100);
+        }
+
+
+        g2.setPaint((Color.BLACK));
+        g2.drawString("Turns count: " + turnsCount, 1000, 120);
+
+        g2.setPaint((Color.BLACK));
+        g2.drawString("Time elapsed: " + timeElapsed / 1000000000 + " seconds", 1000, 140);
+
+
+        g2.setPaint(Color.WHITE);
+
+        for(int i=0; i<nodeList.length; i++){
+            g2.setPaint(board.getNodeColor(i));
+            g2.fillOval(board.getNodeYCoords(i) * interval + 700 - board.getNodeXCoords(i) * interval/2, (int) (board.getNodeXCoords(i) * Math.sqrt(3)/2.0 * interval + 100), diameter, diameter);
+            g2.setPaint(Color.ORANGE);
+            g2.drawString(board.getNodeLabel(i), board.getNodeYCoords(i) * interval + 715 - board.getNodeXCoords(i) * interval/2, (int) (board.getNodeXCoords(i) * Math.sqrt(3)/2.0 * interval + 120));
+        }
+
+    }
+
+    public boolean isWinningCondition() {
+
+
+        if (player1) {       //checking for blue
+            for (int i = 80; i >= 75; i--) {
+                if (!board.getNodeColor(i).equals(Color.BLUE))
+                    return false;
+            }
+            return true;
+        }
+
+        else { //checking for red
+            for (int i = 0; i <= 5; i++) {
+                if (!board.getNodeColor(i).equals(Color.RED))
+                    return false;
+            }
+            return true;
+        }
+
+
+    }
+
+
+    public void clickedForNode(int var1, int var2) {
+
+        for(int i=0; i<81; i++){
+            if (var1 >= board.getNodeYCoords(i) * interval + 700 - board.getNodeXCoords(i) * interval/2
+                    && var1 <= board.getNodeYCoords(i) * interval + 700 - board.getNodeXCoords(i) * interval/2 + diameter
+                    && var2 >= board.getNodeXCoords(i) * (int)(Math.sqrt(3)/2.0 * interval) + 100
+                    && var2 <= board.getNodeXCoords(i) * (int)(Math.sqrt(3)/2.0 * interval) + 100 + diameter) {
+                setMove(i);
+            }
+        }
+
+        System.out.println("PreviousSelectedNode: " + board.getNodeLabel(this.previousSelectedNode) +
+                " SelectNode = " + board.getNodeLabel(this.selectedNode) + " is FirstMove = " + this.firstMove);
+    }
+
+    public void setMove(int chosenNode) {
+        higlight(board.getSecNode(chosenNode));
+        this.previousSelectedNode = this.selectedNode;
+        this.selectedNode = chosenNode;
+
+        ArrayList<Node> stopBugs = board.popularChoice(nodeList[previousSelectedNode]);
+        boolean allgood = false;
+        for(int i=0; i<stopBugs.size(); i++){
+            if(stopBugs.get(i)==nodeList[selectedNode]){
+                allgood = true;
+            }
+        }
+        if(allgood==false){
+            removeOneHighlight(nodeList[previousSelectedNode]);
+        }
+
+        higlight(board.getSecNode(chosenNode));
+
+        if (previousSelectedNode == selectedNode) {
+            removehighlight(nodeList[previousSelectedNode], nodeList[selectedNode]);
+        }
+
+        if (((nodeList[previousSelectedNode].getColor().equals(Color.BLUE) && player1)
+                || (nodeList[previousSelectedNode].getColor().equals(Color.RED) && !player1)
+
+        )) {
+            this.firstMove = false;
+
+
+
+            if (selectedNode != previousSelectedNode && !nodeList[previousSelectedNode].getColor().equals(Color.YELLOW)
+                    && nodeList[selectedNode].getColor().equals(Color.YELLOW)
+            ) {
+
+                nodeList[selectedNode].setColor(nodeList[previousSelectedNode].getColor());
+                removehighlight(nodeList[previousSelectedNode], nodeList[selectedNode]);
+                nodeList[previousSelectedNode].setColor(Color.WHITE);
+                if (isWinningCondition()) { // /!\
+                    if (player1) {
+                        JOptionPane.showMessageDialog(this, currentPlayer + " has won!",
+                                "PLAYER 1 HAS WON", JOptionPane.WARNING_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(this, currentPlayer + " has won!",
+                                "PLAYER 2 HAS WON", JOptionPane.WARNING_MESSAGE);
+                    }
+                }
+
+                if(player1){player1=false;}
+                else{player1 = true;}
+
+                turnsCount++;
+            }
+            else{this.firstMove=true;}
+        }
+        else{this.firstMove=true;}
+
+        repaint();
+
+    }
+
+    public void higlight(Node n) {
+        if (player1 && n.getColor() == Color.BLUE) {
+            ArrayList<Node> tr = board.popularChoice(n);
+            for (int i = 0; i < tr.size(); i++) {
+                tr.get(i).setColor(Color.YELLOW);
+            }
+        } else if (!player1 && n.getColor() == Color.RED) {
+            ArrayList<Node> tr = board.popularChoice(n);
+            for (int i = 0; i < tr.size(); i++) {
+                tr.get(i).setColor(Color.YELLOW);
+            }
+        }
+
+
+    }
+
+    public void removehighlight(Node n, Node m) {
+        ArrayList<Node> tr = board.popularChoice(n);
+        for (int i = 0; i < tr.size(); i++) {
+            if (tr.get(i) == m) {
+
+            } else {
+                tr.get(i).setColor(Color.WHITE);
+            }
+        }
+
+        ArrayList<Node> te = board.popularChoice(m);
+        for (int i = 0; i < tr.size(); i++) {
+
+            tr.get(i).setColor(Color.WHITE);
+
+        }
+    }
+
+    public void removeOneHighlight(Node n){//method that removes the highlight when choosing another node
+        ArrayList<Node> tr = board.popularChoice(n);
+        for(int i=0; i<tr.size(); i++){
+            tr.get(i).setColor(Color.WHITE);
+        }
+    }
+
+
+    class MousePressListener implements MouseListener {
+        MousePressListener() {
+        }
+
+        public void mousePressed(MouseEvent var1) {
+        }
+
+        public void mouseReleased(MouseEvent var1) {
+        }
+
+        public void mouseClicked(MouseEvent var1) {
+            int var2 = var1.getX();
+            int var3 = var1.getY();
+            GUI.this.clickedForNode(var2, var3);
+        }
+
+        public void mouseEntered(MouseEvent var1) {
+        }
+
+        public void mouseExited(MouseEvent var1) {
+        }
+
+    }
+
+
+}
